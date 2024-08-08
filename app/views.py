5e60a2c21e926cad2c2f198ffd7c713c2592ddce
Pinja Savenius <pinja.savenius@student.careeria.fi>
2024-08-08 17:55:51 +0300

    listaukset ja lisäykset toimii

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Supplier, Product
 
 def landingview(request):
@@ -12,7 +12,27 @@ def productlistview(request):
     context = {'products': productlist, 'suppliers': supplierlist}
     return render (request,"productlist.html",context)
 
+def addproduct(request):
+    a = request.POST['productname']
+    b = request.POST['packagesize']
+    c = request.POST['unitprice']
+    d = request.POST['unitsinstock']
+    e = request.POST['supplier']
+    
+    Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, supplier = Supplier.objects.get(id = e)).save()
+    return redirect(request.META['HTTP_REFERER'])
+
 def supplierlistview(request):
     supplierlist = Supplier.objects.all()
     context = {'suppliers': supplierlist}
     return render (request, "supplierlist.html",context)
+
+def addsupplier(request):
+    a = request.POST['companyname']
+    b = request.POST['contactname']
+    c = request.POST['address']
+    d = request.POST['phone']
+    e = request.POST['email']
+    f = request.POST['country']
+    Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
+    return redirect(request.META['HTTP_REFERER'])
